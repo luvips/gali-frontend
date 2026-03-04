002b897c8e4ce5c9f5521f5cb575155a5b65fb93
luvia <luviaamagaali07@gmail.com>
2026-03-04 17:05:11 -0600

    crea layout principal

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/ui/Navbar";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "GALI",
+  description: "Descubre las mejores peliculas",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={`${poppins.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
